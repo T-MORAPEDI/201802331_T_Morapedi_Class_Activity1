e532ca2f3a0f1b64cd8b41d8130d164dc5a583f7
T.D MORAPEDI <101623837+T-MORAPEDI@users.noreply.github.com>
2023-07-29 21:55:18 +0200

    Update script.py

--- script.py
+++ script.py
@@ -29,7 +29,7 @@ y = df_encoded['sales']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=0)
 
-# Creating the Ridge Regression model and fitting the data
+# Creating the Lasso Regression model and fitting the data
 model = linear_model.Lasso(alpha=.5)
 model.fit(X_train, y_train)
 
